3df9db0d33d97be0d540092f1ae08f51d2e41830
Danila Poyarkov <dev@dannote.net>
2026-04-24 13:36:00 +0300

    Update precompiled NIF checksums for v0.2.3

--- checksum-Elixir.Folio.Native.exs
+++ checksum-Elixir.Folio.Native.exs
@@ -1,5 +1,6 @@
-# This file will be auto-generated after the precompile CI workflow runs.
-# Run: mix rustler_precompiled.download Folio.Native --all --print
-# Then commit the generated file.
 %{
+  "libfolio_nif-v0.2.3-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:131d6c311160b465b786f476105794cb42975be1b898c0deb45b5379622412d9",
+  "libfolio_nif-v0.2.3-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:b7bda5a3181f7fa597db58e414c4d94ee2870cc6a9bc17bbce5069d079df375d",
+  "libfolio_nif-v0.2.3-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:a6ad811e264aee0ad3100ef0794f2753bbf4cae5bb01dca86bceaac00ee10819",
+  "libfolio_nif-v0.2.3-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:603810cb50e6a85c6e19a80e018a0e331d66b7e4d3891682adddef6ba0032790",
 }
